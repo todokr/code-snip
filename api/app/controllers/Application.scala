30e7adf82b4e1c6f88388e1b0508b1c106e888ee
田所駿佑 <Shunsuke@tadokoroshunyuu-no-iMac.local>
2015-08-15 01:50:48 +0900

    remove unnecessary  code

--- api/app/controllers/Application.scala
+++ api/app/controllers/Application.scala
@@ -5,11 +5,6 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
   def options(path:String) = Action { Ok("")}
-
 
 }
