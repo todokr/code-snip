189fbd720040a9f8e8ea33d8e7c0861f90894d30
Shunsuke Tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2016-07-30 17:26:41 +0900

    Refine checking auth feature

--- api/app/auth/AuthAction.scala
+++ api/app/auth/AuthAction.scala
@@ -12,14 +12,7 @@ import scala.concurrent.Future
 
 object AuthAction extends ActionBuilder[Request]{
 
-  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
-    Logger.debug("beforeAction is invoked.")
-    request.session.get("auth") match {
-      case Some(_) => block.apply(request)
-      case None => {
-        Logger.warn("not authorized access tried")
-        Future.successful(Results.Status(401).apply(Json.obj("result" -> "Not Authorized")))
-      }
-    }
+  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = 
+    request.session.get("auth") map (_ => block(request)) getOrElse Future.successful(Unauthorized)
   }
 }
